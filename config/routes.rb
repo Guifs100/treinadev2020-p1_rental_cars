bbf8e843f56e2e2ab8a113b6bd6ef124e1f0fac4
Guifs <guifs100@gmail.com>
2020-04-25 19:51:21 -0300

    sessão 04

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # get '/', to: 'home#index'
   root 'home#index'
 
-  resources :manufacturers, only: [:index, :show, :new, :create]
+  resources :manufacturers, only: [:index, :show, :new, :create, :edit, :update]
 end
